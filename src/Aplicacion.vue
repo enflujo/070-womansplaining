3aeb9bef4cf17958ca6cc03559d4113da25c5972
Antonia Bustamante <bustamanter.antonia@gmail.com>
2022-09-19 16:59:38 -0500

    Initial commit

---
+++ src/Aplicacion.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Hola from './componentes/Hola.vue';
+</script>
+
+<template>
+  <Hola />
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/componentes/Hola.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <h1>Plantilla Vue EnFlujo</h1>
+</template>
+
+<style lang="scss" scoped></style>
