9beb97cbb7479211a2b9bdf11a4ba79a3a765f91
anattolia <bustamanter.antonia@gmail.com>
2022-11-24 09:46:24 -0500

    ⚗️ no funciona

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
+import { fileURLToPath, URL } from 'url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  clearScreen: false,
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   build: {
     outDir: './docs',
     emptyOutDir: true,
   },
+  base: '/womansplaining/',
 });
